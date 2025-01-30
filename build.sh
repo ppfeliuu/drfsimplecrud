066a94406d0cd2d7df731fb002cc319d19a1ecf2
D129563 <pedro.feliu@entelgy.com>
2025-01-30 21:21:43 +0000

    Fix req2

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+python -m pip install --upgrade pip
+
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
